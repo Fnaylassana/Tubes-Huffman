47c499793dcd5df28356d4304c108374c4d92eae
nisrinawafaz <nisrinawafaz@gmail.com>
2023-04-15 14:31:10 +0700

    push header.h

---
+++ header.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#define Nil NULL
+#define Info(P) (P)->info
+#define Freq(P) (P)->freq
+#define LeftSon(P) (P)->leftson
+#define RightSon(P) (P)->rightson
+#define Parent(P) (P)->parent
+#define Prev(P) (P)->prev
+#define Next(P) (P)->next
+#define infotype char*
+
+typedef struct node *address;
+struct node {
+
+	char info;
+	int freq;
+	address prev;
+	address next;
+}Node;
+
+typedef struct NodeTree *Link;
+typedef struct NodeTree {
+	char info;
+	int freq;
+	Link leftson, rightson, parent;
+} nodeTree;
+
+typedef struct LeafRef *address1;
+typedef struct LeafRef {
+	address1 prev, next;
+	Link info;
+} leafref;
